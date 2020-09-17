df9ff93a69eaf8741742e8d697bd326a68dda33c
Gus Porter <apor417@aucklanduni.ac.nz>
2020-09-17 13:00:09 +1200

    Added prettier settings

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  singleQuote: false,
+  semi: true,
+  useTabs: false,
+  tabWidth: 3,
+  arrowParens: 'always',
+  trailingComma: 'none'
+};
+
+// npx prettier --config .prettierrc.js --write "src/**"
